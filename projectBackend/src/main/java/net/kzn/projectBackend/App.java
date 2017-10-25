48e6dc296df516005879427f339249c28a59bb1c
lobnahariz <lobnahariz@gmail.com>
2017-10-25 11:22:03 +0100

    comm

---
+++ projectBackend/src/main/java/net/kzn/projectBackend/App.java
@@ -0,0 +1,13 @@
+package net.kzn.projectBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
